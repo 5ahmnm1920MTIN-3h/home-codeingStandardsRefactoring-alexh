1be36ed4756a5607dda5ca533cf3bde576779048
alexhoedlmoser <alex.hoedlmoser@gmail.com>
2019-12-14 17:03:23 +0100

    refactoring Obstacle.cs: removed unnecessary comments because code is self-explanatory

--- Assets/MyGame/Scripts/Obstacle.cs
+++ Assets/MyGame/Scripts/Obstacle.cs
@@ -11,22 +11,13 @@ public class Obstacle : MonoBehaviour
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //if obstacle's position x is < -15f it will be destroyed
         if(transform.position.x < -15f)
         {
             Destroy(gameObject);
         }
 
-        //if obstacle's position x is < -15f it will be destroyed
         if (transform.position.x > 15f)
         {
             Destroy(gameObject);
