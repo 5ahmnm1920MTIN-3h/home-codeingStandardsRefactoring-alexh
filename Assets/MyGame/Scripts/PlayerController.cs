e1e6bb0b3df725af01683da97299af5c0be16d2a
alexhoedlmoser <alex.hoedlmoser@gmail.com>
2019-12-14 17:40:23 +0100

    refactoring PlayerController.cs: removed/added spaces and moved all global variable declarations to the top for increased readability

--- Assets/MyGame/Scripts/PlayerController.cs
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -2,10 +2,12 @@
 
 public class PlayerController : MonoBehaviour
 {
+
     Rigidbody2D rb;
     Animator anim, anim2, anim3, anim4, anim5;
     [SerializeField] float jumpForce;
-    
+    bool grounded;
+    bool gameOver = false;
 
     private void Awake()
     {
@@ -28,24 +30,14 @@ public class PlayerController : MonoBehaviour
             {
                 jump();
             }
-
-
-
-
         }
     }
-
-    bool grounded;
-    bool gameOver = false;
 
     void jump()
     {
         grounded = false;
-
         rb.velocity = Vector2.up * jumpForce;
-
         anim.SetTrigger("Jump");
-
         GameManager.instance.IncrementScore();
         Debug.Log("DeleteMe");
     }
@@ -55,23 +47,22 @@ public class PlayerController : MonoBehaviour
         return true;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)   {
+    private void OnCollisionEnter2D(Collision2D collision)   
+    {
         if(collision.gameObject.tag == "Ground")
         {
-            grounded = true;}
+            grounded = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Obstacle"){
+        if(collision.gameObject.tag == "Obstacle")
+        {
             GameManager.instance.GameOver();
             Destroy(collision.gameObject);
             anim.Play("SantaDeath");
             gameOver = SetGameOverTrue();
         }
     }
-
-
-
-
 }
